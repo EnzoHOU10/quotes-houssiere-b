33e5eef9f10ec4bca638136c31add13c1f7a532e
EnzoHOU10 <129770379+EnzoHOU10@users.noreply.github.com>
2023-05-26 08:56:56 +0200

    Correction

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def main():
         
         if choice == "1":
             print_quote(random_quote(quotes))
-        elif choice == 2
+        elif choice == "2":
     		count = int(input("Enter the number of quotes to display: "))
     		display_quotes(quotes, count)
         elif choice == "3":
